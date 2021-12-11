e48fbb83bae07ae295d0a6fda954e46f62a7d3f3
HaiNguyen2903 <hainguyen29031412@gmail.com>
2021-12-11 15:19:46 +0700

    change view resolution

--- config.py
+++ config.py
@@ -2,4 +2,5 @@ SUPPORT_FORMAT=['zip']
 
 SAVE_FOLDER = '/Users/hainguyen/Documents/deep_learning_projects/streamlit/save_dir'
 
-VIEW_SHAPE = (1920, 1080)
+# VIEW_SHAPE = (1920, 1080)
+VIEW_SHAPE = (640, 480)
